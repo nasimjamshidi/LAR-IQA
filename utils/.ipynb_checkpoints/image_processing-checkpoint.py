68c741d2bd7d1aa20db70e54731980138ac252c6
Nasim Jamshidi Avanaki <36204270+nasimjamshidi@users.noreply.github.com>
2025-01-29 11:26:43 +0100

    Delete utils/.ipynb_checkpoints directory

--- utils/.ipynb_checkpoints/image_processing-checkpoint.py
+++
@@ -1,7 +0,0 @@
-from PIL import Image
-import torch
-from torchvision import transforms
-
-def read_image(img_name, transform):
-    image = Image.open(img_name).convert('RGB')
-    return transform(image)
